3c31dab0349669960bea9f20cc4af70dda8c6d54
IVadymMarchenko <topim31@gmail.com>
2024-05-05 23:41:26 +0300

    vadym_app_photo

--- app_jull/app_photo/forms.py
+++ app_jull/app_photo/forms.py
@@ -1,11 +1,21 @@
+from django.core.exceptions import ValidationError
 from django.forms import ModelForm, CharField, ImageField, TextInput, FileInput, FileField
 
 from .models import Picture
 
 
+def validate_file_size(value):
+    # Максимальный размер файла в байтах (10 МБ)
+    max_size = 10 * 1024 * 1024  # 10 МБ
+
+    if value.size > max_size:
+        raise ValidationError(f"Максимальный размер файла - 10 МБ. Ваш файл слишком большой ({value.size} байт).")
+
+
+
 class FormPicture(ModelForm):
     description = CharField(max_length=200,widget=TextInput(attrs={'class':'form-control', 'id':'exampleInputEmail1'}))
-    image = FileField(widget=FileInput(attrs={'class':'form-control', 'id':'formFile'}))
+    image = FileField(widget=FileInput(attrs={'class':'form-control', 'id':'formFile'}),validators=[validate_file_size])
 
     class Meta:
         model = Picture
