60fccc23be96469d68d25509366c12c71d488b9e
IVadymMarchenko <topim31@gmail.com>
2024-05-07 23:40:13 +0300

    photo100

--- app_jull/app_photo/models.py
+++ app_jull/app_photo/models.py
@@ -8,4 +8,4 @@ from cloudinary.models import CloudinaryField
 
 class Picture(models.Model):
     description = models.CharField(max_length=200)
-    image = models.FileField(upload_to='photo_for_users',max_length=200)
+    image = models.FileField(upload_to='photo_for_users',max_length=500)
